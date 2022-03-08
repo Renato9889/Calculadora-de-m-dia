6fa26cfaaadd67ebcf8cf752eea184abe854480b
Renato9889 <38532053+Renato9889@users.noreply.github.com>
2022-03-08 17:35:43 -0300

    Add files via upload

---
+++ calculadora-de-media.markdown
@@ -0,0 +1,7 @@
+Calculadora de média
+--------------------
+
+
+A [Pen](https://codepen.io/renato9889/pen/QWORJKL) by [Renato9889](https://codepen.io/renato9889) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/license/pen/QWORJKL).
---
+++ script.js
@@ -0,0 +1,11 @@
+var nome = "Renato"
+
+var notaPrimeiroBimestre = 9
+var notaSegundoBimestre = 6
+var notaTerceiroBimestre = 8
+var notaQuartoBimestre = 5
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre
++ notaTerceiroBimestre + notaQuartoBimestre) / 4
+
+console.log("Bem-Vindo "+nome)
+console.log(notaFinal)

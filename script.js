91e641b86b0141c8d44866b6682f8a022f556fb7
Renato9889 <38532053+Renato9889@users.noreply.github.com>
2022-03-08 21:45:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var nome = "Renato"
 
-var notaPrimeiroBimestre = 9
-var notaSegundoBimestre = 6
-var notaTerceiroBimestre = 8
-var notaQuartoBimestre = 5
+var notaPrimeiroBimestre = 9.56
+var notaSegundoBimestre = 6.98
+var notaTerceiroBimestre = 5.34
+var notaQuartoBimestre = 5.99
 var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre
 + notaTerceiroBimestre + notaQuartoBimestre) / 4
 
+var notaFinalFixada = notaFinal.toFixed(1)
+
 console.log("Bem-Vindo "+nome)
-console.log(notaFinal)
+console.log(notaFinalFixada)
+
+//primeira aula
